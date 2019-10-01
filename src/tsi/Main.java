62ccb115762efc3f99670f723344102480faa06a
MartinsCernevskis <m.cernevskis@gmail.com>
2019-10-01 20:51:28 +0300

    return if numbers do not match

--- src/tsi/Main.java
+++ src/tsi/Main.java
@@ -1,5 +1,6 @@
 package tsi;
 
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -11,18 +12,21 @@ public class Main {
     public static void main(String[] args) {
 
         String answer;
+        String username;
 
         do {
+            username = getUserName();
+
             boolean userWon = false;
 
             for (int i = 0; i < 10; i++) {
                 int myNum = rand.nextInt(100) + 1;
-//                    System.out.println(myNum);
-                System.out.println("enter number");
-                int userNum = scan.nextInt();
+
+                int userNum = askInt("\nPlease enter your guess", 1, 100);
 
                 if (userNum < myNum) {
                     System.out.println("Try higher number");
+
                 } else if (userNum == myNum) {
                     System.out.println("Congratulations!!!!! Your number is correct");
                     userWon = true;
@@ -32,15 +36,45 @@ public class Main {
                 }
             }
 
-            if (!userWon) System.out.print("\nLooser");
-            System.out.println("\nDo you want to continue?\n1: yes\n2: no");
-            answer = scan.next();
+            if (!userWon) System.out.print("\nYou loose");
 
-        } while (answer.equals("1"));
+        } while (askYesNo("\nDo You want to play again? (y/n)"));
     }
 
+    static int askInt(String msg, int min, int max) {
+        while (true) {
+            try {
+                System.out.print(msg);
+                int answer = scan.nextInt();
+                if (answer >= min && answer <= max) {
+                    return answer;
+                }
+            } catch (InputMismatchException ex) {
+                System.out.println("\nit is not a number, please try again");
+                scan.next();
+            }
+            System.out.printf("Please enter number from %d to %d\n", min, max);
+        }
+    }
 
+    static boolean askYesNo(String msg) {
+        while (true) {
+            System.out.print(msg);
+            String answer = scan.next();
+            boolean isY = answer.equalsIgnoreCase("y");
+            boolean isN = answer.equalsIgnoreCase("n");
+            if (isY || isN) {
+                return isY;
+            }
+            System.out.printf("Enter 'Y' or 'N'");
+        }
+    }
+
+    static String getUserName() {
+        System.out.println("Enter Your name please");
+
+        String username = scan.nextLine();
+        System.out.print("Hello, " + username);
+        return username;
+    }
 }
-
-
-
