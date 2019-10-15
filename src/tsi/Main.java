d605752e5f1ed887982db919035680ca898b7f6e
MartinsCernevskis <m.cernevskis@gmail.com>
2019-10-15 20:36:13 +0300

    time count final with user top display with showResults2 method. writes to file user details

--- src/tsi/Main.java
+++ src/tsi/Main.java
@@ -1,5 +1,9 @@
 package tsi;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.*;
 
 public class Main {
@@ -8,11 +12,16 @@ public class Main {
     static Scanner scan = new Scanner(System.in);
     static ArrayList<GameResult> LeaderBoard = new ArrayList<>();
 
+    public Main() throws FileNotFoundException {
+    }
+
+
     public static void main(String[] args) {
 
         String answer;
         String username;
         float elapsedTimeMillis = 0;
+        loadResults();
 
         do {
             username = getUserName();
@@ -20,12 +29,14 @@ public class Main {
             boolean userWon = false;
             int myNum = rand.nextInt(100) + 1;
 
+            long start = System.currentTimeMillis(); // START counter !!!!!
+
             for (int i = 0; i < 10; i++) {
 
 
                 int userNum = askInt("\nPlease enter your guess", 1, 100);
 
-                long start = System.currentTimeMillis(); // START counter !!!!!
+
 
                 if (userNum < myNum) {
                     System.out.println(myNum);
@@ -37,7 +48,7 @@ public class Main {
 
                     GameResult r = new GameResult(); // r = obj reference
                     r.setName(username);
-                    r.setTriesCount(i + 1) ;
+                    r.setTriesCount(i + 1);
                     r.setGameplay(end - start);
 
                     LeaderBoard.add(r);
@@ -48,8 +59,6 @@ public class Main {
                     break;
                 } else {
                     System.out.println("Try lower number");
-
-
                 }
             }
 
@@ -57,36 +66,79 @@ public class Main {
 
         } while (askYesNo("\nDo You want to play again? (y/n)"));
 
-        showResults2();
 
+        showResults2();
+        save();
 
         System.out.println();
         System.out.println("Goodbye");
     }
 
-    private static void showResults() {
-        LeaderBoard.sort(Comparator.comparingInt(GameResult::getTriesCount).thenComparing(GameResult::getGameplay));
+    private static void loadResults() {
+        File file = new File("leader_board.txt");
+        try(Scanner in = new Scanner(file)){
+            while(in.hasNext()){
 
-        for (GameResult result : LeaderBoard){
-            System.out.printf("%s \t\t\t  %d %d\n" ,
-                    result.getName(),
-                    result.getTriesCount(),
-                    result.getGameplay()/1000);
+                GameResult r = new GameResult();
+
+                String name = in.next();
+                int tries = in.nextInt();
+                long time = in.nextLong();
+
+                r.setName(name);
+                r.setTriesCount(tries);
+                r.setGameplay(time);
+
+                LeaderBoard.add(r);
+
+            }
+        }catch (IOException e){
+            System.out.println("Cannot read leader board");
         }
     }
 
-    private static void showResults2(){
+    private static void save() {
+        File file = new File("leader_board.txt");
+        try(PrintWriter out = new PrintWriter(file)) {
+            for (GameResult result : LeaderBoard) {
+            out.printf("%s %d %d\n",
+                    result.getName(),
+                    result.getTriesCount(),
+                    result.getGameplay());
+        }
+        }catch(IOException e){
+
+        }
+    }
+
+
+//    private static void showResults() {
+//        LeaderBoard.sort(Comparator.comparingInt(GameResult::getTriesCount).thenComparing(GameResult::getGameplay));
+//
+//        for (GameResult result : LeaderBoard) {
+//            System.out.printf("%s \t\t\t  %d %d\n",
+//                    result.getName(),
+//                    result.getTriesCount(),
+//                    result.getGameplay() / 1000);
+//        }
+//    }
+
+
+
+
+    private static void showResults2() {
         LeaderBoard.stream()
-                .sorted( Comparator
+                .sorted(Comparator
                         .comparingInt(GameResult::getTriesCount)
                         .thenComparing(GameResult::getGameplay))
                 .limit(5)
-                .forEach(r -> System.out.printf("%s \t\t\t  %d %d\n" ,
+                .forEach(r -> System.out.printf("%s \t\t\t  %d %d\n",
                         r.getName(),
                         r.getTriesCount(),
-                        r.getGameplay()/1000));
+                        r.getGameplay() / 1000));
 
     }
+
     static int askInt(String msg, int min, int max) {
         while (true) {
             try {
